80cfcce5f6ebde81dd719f676f6b60a177b55dda
Jona Libonati <libonati.jonathan@gmail.com>
2024-02-19 18:43:51 -0300

    Launch book hero

--- src/components/overview/BookSection/BookSection.jsx
+++ src/components/overview/BookSection/BookSection.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import image from '../../../assets/images/competencia_presentacion_fecha.jpg'
-import { SvgIcon } from '../../SvgIcon/SvgIcon';
+import image from '../../../assets/images/competencia_presentacion_libro.jpg'
 import './bookSection.styles.css';
 
 export const BookSection = () => {
@@ -8,12 +7,8 @@ export const BookSection = () => {
     <>
         <section className='book' >
             <div className='book__container' >
-                <img className='book__img' src={image} />
+                <div className='book__img' ></div>
             </div>
-            <div className="book__see">
-							&nbsp;<a className="book__button" href="https://www.cpau.org/nota/37029" target="_blank">inscribete aqui</a>
-							<SvgIcon xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="about__chevron" viewBox="0 0 16 16" paths={[{fillRule:"evenodd", d:"M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z",}]} />
-						</div>
         </section>
     </>
   )
